39e7915ad13595cc13ba61dfa935585f56626cf5
Jessica Vidal <vidaljessi@gmail.com>
2020-11-25 12:39:46 +0100

    add validation for balance and unconfirmedbalance

--- src/main/scala/chains/ChainBitCoinB.scala
+++ src/main/scala/chains/ChainBitCoinB.scala
@@ -4,11 +4,21 @@ import baseConfig.LoadConfiguration
 import io.gatling.core.Predef._
 import services.ServiceNodeB
 
-object ChainBitCoinB{
+object ChainBitCoinB {
 
   val nodeB = LoadConfiguration.baseUrlNodeB
   var authorization = "Basic bm9kZUI6c2VjcmV0cGFzc3dvcmQy"
 
   val createNewAddress = feed(Array(Map("AUTHORIZATION_NODE_B"-> authorization)).circular)
-  .exec(ServiceNodeB.getnewaddress(nodeB))
+    .exec(ServiceNodeB.getnewaddress(nodeB))
+    .feed(Array(Map("AUTHORIZATION_NODE"-> authorization)).circular)
+    .exec(ServiceNodeB.getbalance(nodeB))
+    .exec(ServiceNodeB.getActualBalance(nodeB))
+
+  val verifyTransferReceive = feed(Array(Map("AUTHORIZATION_NODE"-> authorization)).circular)
+    .doWhile(session => session("UNCONFIRMED_BALANCE").as[Double] > 0.00000000){
+      exec(ServiceNodeB.validateUnconfirmBalance(nodeB))
+    }
+    .exec(ServiceNodeB.getbalance(nodeB))
+  .exec(ServiceNodeB.verifyBalance(nodeB))
 }
--- src/main/scala/services/ServiceBitCoin.scala
+++ src/main/scala/services/ServiceBitCoin.scala
@@ -1,12 +1,14 @@
 package services
 
-import baseConfig.BodiesFiles
+import baseConfig.{BodiesFiles, CustomLogging}
+import chains.ChainBitCoinB.nodeB
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class ServiceBitCoin {
+class ServiceBitCoin extends CustomLogging{
 
-  def generatetoaddress(node: String) = exec(http("generatetoaddress")
+  def generatetoaddress(node: String,nblocks:Int) = feed(Array(Map("N_BLOCKS"-> nblocks)).circular)
+    .exec(http("generatetoaddress")
     .post(node)
     .header("Authorization","${AUTHORIZATION_NODE}")
     .body(ElFileBody(BodiesFiles.generatetoaddress)).asJson
@@ -25,7 +27,42 @@ class ServiceBitCoin {
     .post(node)
     .header("Authorization","${AUTHORIZATION_NODE}")
     .body(ElFileBody(BodiesFiles.sendtoaddress)).asJson
-    .check(status.is(200))
-    .check(jsonPath("$.result").find.saveAs("BALANCE")))
+    .check(status.is(200)))
     .exitHereIfFailed
+
+  def getActualBalance(node:String) = exec(session => {
+    val actualBalanceNode = session("BALANCE").as[Double]
+    session.set("ACTUAL_BALANCE",actualBalanceNode)
+  })
+
+  def verifyBalance(node:String) = exec(session => {
+    val newBalanceNode = session("BALANCE").as[Double]
+    val oldBalanceNode = session("ACTUAL_BALANCE").as[Double]
+    val userId = session.userId
+    consoleLogger.info(">>> USER ID:%s".format(userId))
+    consoleLogger.info(">>> NODE:%s".format(node))
+    consoleLogger.info(">>> OLD_BALANCE is %s: ".format(oldBalanceNode))
+    consoleLogger.info(">>> NEW_BALANCE is %s: ".format(newBalanceNode))
+    if(oldBalanceNode<=newBalanceNode)
+      session.markAsFailed
+    session
+  })
+
+  def getunconfirmedbalance(node: String) = exec(http("getunconfirmedbalance")
+    .post(node)
+    .header("Authorization","${AUTHORIZATION_NODE}")
+    .body(ElFileBody(BodiesFiles.getunconfirmedbalance)).asJson
+    .check(status.is(200))
+    .check(jsonPath("$.result").find.saveAs("UNCONFIRMED_BALANCE")))
+    .exitHereIfFailed
+
+  def validateUnconfirmBalance(node: String) = exec(ServiceNodeB.getunconfirmedbalance(nodeB))
+  .exec(session => {
+    val unconfirmedbalance = session("UNCONFIRMED_BALANCE").as[Double]
+    val userId = session.userId
+    consoleLogger.info(">>> USER ID:%s".format(userId))
+    consoleLogger.info(">>> NODE:%s".format(node))
+    consoleLogger.info(">>> UNCONFIRMED BALANCE:%s".format(unconfirmedbalance))
+    session
+  })
 }
